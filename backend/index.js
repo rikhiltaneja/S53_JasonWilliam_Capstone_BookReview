67e3dbbe2fca2d2e877d2a758d9eb835a57ab9b3
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-03-18 11:16:53 +0530

    ConnectedDB and Created dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
 
+
+mongoose.connect(
+    process.env.MONGOURI
+)
+.then(() => {
+    console.log('Connected to MongoDB')
+}).catch(err => {
+    console.log(err)
+})  
+     
 const app = express();
 
 
