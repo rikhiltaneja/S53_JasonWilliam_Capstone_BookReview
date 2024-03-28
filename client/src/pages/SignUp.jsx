99e1eb7ec267725dafdce9590a54923fad1f740c
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-03-28 18:04:09 +0530

    created signup with functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,46 +1,125 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function SignUp() {
+
+
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+    
+      if (res.status === 400) {
+        toast.error('User already exists');
+      } else {
+        const data = await res.json();
+        setLoading(false);
+        if (res.ok) {
+          navigate('/sign-in');
+        } else {
+          setErrorMessage(data.message || 'Something went wrong.');
+        }
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+    
+  };
   return (
-    <div className='min-h-screen mt-20 '>
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5 ">
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-16'>
         {/* left */}
-        <div className="flex-1">
+        <div className='flex-1'>
         <Link to='/' className='font-bold dark:text-white text-4xl'>
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white mr-4'>BookReview</span>
-      </Link>
-      <p className='text-sm mt-5'>
-      Dive deeper with exclusive books and insights. Sign up now! 
-      </p>
+        </Link>
+          <p className='text-sm mt-5'>
+          Dive deeper with exclusive books and insights. Sign up now! 
+          </p>
         </div>
         {/* right */}
-        <div className="flex-1">
-          <form>
-            <div className="flex flex-col gap-4">
-              <Label value='Your Username'/>
-              <TextInput id='username' type='text' placeholder='Username' aria-label='Username'  />
-              </div>
-              <div className="">
-              <Label value='Your Email'/>
-              <TextInput id='email' type='email' placeholder='Email' aria-label='Email'/>
+
+        <div className='flex-1'>
+          <form className='flex flex-col gap-6' onSubmit={handleSubmit}>
+            <div>
+              <Label value='Your username' />
+              <TextInput
+                type='text'
+                placeholder='Username'
+                id='username'
+                aria-label='Username'
+                onChange={handleChange}
+              />
             </div>
-              <div className="">
-              <Label value='Your Password'/>
-              <TextInput id='password' type='password' placeholder='Password' aria-label='Password' />
+            <div>
+              <Label value='Your email' />
+              <TextInput
+                type='email'
+                placeholder='name@company.com'
+                id='email'
+                aria-label='Username'
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit' className=' w-96 flex-col gap-4 mt-5'>Sign Up</Button>
+            <div>
+              <Label value='Your password' />
+              <TextInput
+                type='password'
+                placeholder='Password'
+                id='password'
+                aria-label='Username'
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
+            </Button>
+            
           </form>
-          <div className="">
-            <span>
-            Have an account? <Link to='/sign-in' className='text-blue-500 cursor-pointer'>Sign In</Link>
-            </span>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account?</span>
+            <Link to='/sign-in' className='text-blue-500'>
+              Sign In
+            </Link>
           </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
-
-export default SignUp
