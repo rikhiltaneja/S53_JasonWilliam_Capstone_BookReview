dcea9499a523d2061cc09ce7e616c731e0ebba70
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-05-07 11:34:47 +0530

    created signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,7 +5,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/s
 import { app } from '../firebase'
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart,updateSuccess,updateFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure } from '../redux/user/userSlice';
+import { updateStart,updateSuccess,updateFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure ,signoutSuccess} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import {HiOutlineExclamationCircle} from 'react-icons/hi'
 import {  toast } from 'react-toastify';
@@ -128,6 +128,23 @@ const handleDeleteUser = async () => {
   }
 };
 
+const handleSignout = async () => {
+  try {
+    const res = await fetch('/api/user/signout', {
+      method: 'POST',
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      console.log(data.message);
+    } else {
+      dispatch(signoutSuccess());
+      toast.success("Successfully signed out")
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
@@ -178,7 +195,7 @@ const handleDeleteUser = async () => {
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick={() => setShowModal(true)} className="cursor-pointer">Delete Account</span>
-        <span className="cursor-pointer">Sign Out</span>
+        <span onClick={handleSignout} className="cursor-pointer">Sign Out</span>
     </div>
     {updateUserSuccess && (
         <Alert color='success' className='mt-5'>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,12 +6,31 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon , FaSun,} from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice.js';
+import { signoutSuccess } from '../redux/user/userSlice.js';
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Header() {
   const path = useLocation().pathname;
   const dispatch = useDispatch()
   const {currentUser} = useSelector((state) => state.user);
   const {theme} = useSelector((state)=>state.theme);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+        toast.success("Successfully signed out")
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Navbar className='border-b-2'>
       <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white '>
@@ -53,7 +72,7 @@ function Header() {
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            <Dropdown.Item >Sign out</Dropdown.Item>
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
 
           </Dropdown>
         ): 
